d847b55c4cb8290e419faa4adca77fc66a75d8f7
Candost Dagdeviren <candostdagdeviren@gmail.com>
2023-07-30 08:50:45 +0200

    Change Blog Title

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Candost's Space";
+export const SITE_TITLE = "Candost's Blog";
 export const SITE_DESCRIPTION =
-  "Welcome to Candost's Space. I write about multiple aspects and constraints of the complicated life.";
+  "Welcome to Candost's Blog. I write about multiple aspects and constraints of the complicated life.";
 export const TWITTER_HANDLE = "@candosten";
 export const MY_NAME = "Candost Dagdeviren";
 
