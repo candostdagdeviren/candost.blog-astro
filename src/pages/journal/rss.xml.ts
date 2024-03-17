1ff9189d4ded1cabf45075ecb1054860e8e67b1c
Candost Dagdeviren <candostdagdeviren@gmail.com>
2024-03-17 08:18:36 +0100

    Clean up

--- src/pages/journal/rss.xml.ts
+++ src/pages/journal/rss.xml.ts
@@ -1,7 +1,7 @@
 import rss from "@astrojs/rss";
 import { blog } from "../../lib/markdoc/frontmatter.schema";
 import { readAll } from "../../lib/markdoc/read";
-import { SITE_TITLE, SITE_URL } from "../../config";
+import { SITE_URL } from "../../config";
 import Markdoc from "@markdoc/markdoc";
 
 export async function GET() {
--- src/pages/newsletter/rss.xml.ts
+++ src/pages/newsletter/rss.xml.ts
@@ -1,7 +1,7 @@
 import rss from "@astrojs/rss";
 import { newsletters } from "../../lib/markdoc/frontmatter.schema";
 import { readAll } from "../../lib/markdoc/read";
-import { SITE_TITLE, SITE_URL } from "../../config";
+import { SITE_URL } from "../../config";
 import Markdoc from "@markdoc/markdoc";
 
 export async function GET() {
