83cc6048c32f900ef3c2b16cd8794d1be63eaf8c
Candost Dagdeviren <candostdagdeviren@gmail.com>
2025-11-23 08:06:08 +0100

    Update main RSS feed to include content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,21 @@
 import rss from '@astrojs/rss';
 import {site} from "../consts";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 import {getAllContent} from "../utils/getAllContent";
 import {sortPostsByDate} from "../utils/sortPostsByDate";
+const parser = new MarkdownIt();
 
-export async function GET(context) {
+export async function GET() {
   const blog = await getAllContent();
+
+  let baseUrl = site.url;
+  // removing trailing slash if found
+  // https://example.com/ => https://example.com
+  baseUrl = baseUrl.replace(/\/+$/g, "");
+
   const sortedBlog = sortPostsByDate(blog);
+
   return rss({
     title: site.title,
     description: site.description,
@@ -15,9 +25,10 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description? post.data.description : "",
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/posts/[slug]` routes
-      link: post.collection == 'posts' ? `/${post.slug}/` : `/${post.collection}/${post.slug}/`,
+      link: post.collection == 'posts' ? `${baseUrl}/${post.slug}/` : `${baseUrl}/${post.collection}/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
   });
 }
--- src/utils/getAllContent.ts
+++ src/utils/getAllContent.ts
@@ -1,19 +1,12 @@
 import { getCollectionByName } from "./getCollectionByName";
 
 export const getAllContent = async () => {
-  let posts = await getCollectionByName('posts');
-  let journal = await getCollectionByName('journal');
-  let newsletter = await getCollectionByName('newsletter');
-  let notes = await getCollectionByName('notes');
-  let books = await getCollectionByName('books');
-  let de = await getCollectionByName('de');
-  let podcast = await getCollectionByName('podcast');
-  const all = [...posts, ...journal, ...newsletter, ...notes, ...books, ...de, ...podcast];
-  if (all && all.length > 0 ) {
-    return all.filter(({data}) => {
-      return import.meta.env.PROD ? !data.draft : true
-    });
-  } else {
-    return []
-  }
+  let posts = await getCollectionByName("posts");
+  let journal = await getCollectionByName("journal");
+  let newsletter = await getCollectionByName("newsletter");
+  let notes = await getCollectionByName("notes");
+  let books = await getCollectionByName("books");
+  let de = await getCollectionByName("de");
+  let podcast = await getCollectionByName("podcast");
+  return [...posts, ...journal, ...newsletter, ...notes, ...books, ...de, ...podcast];
 }
